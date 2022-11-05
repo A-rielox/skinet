593780d8fd50418baf44b9f278be8bee0645b387
A-rielox <arielox.ag@gmail.com>
2022-11-05 00:05:47 -0600

    hasta 42

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -42,8 +42,11 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            var product = await _productsRepo.GetByIdAsync(id);
-            
+            //var product = await _productsRepo.GetByIdAsync(id);
+
+            var spec = new ProductsWithTypesAndBrandsSpecification(id);
+            var product = await _productsRepo.GetEntityWithSpec(spec);
+
             return product;
         }
 
--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -1,10 +1,18 @@
 ﻿using Core.Entities;
+using System.Linq.Expressions;
 
 namespace Core.Specifications
 {
     public class ProductsWithTypesAndBrandsSpecification : BaseSpacification<Product>
     {
         public ProductsWithTypesAndBrandsSpecification()
+        {
+            AddInclude(x => x.ProductType);
+            AddInclude(x => x.ProductBrand);
+        }
+
+        public ProductsWithTypesAndBrandsSpecification(int id) 
+            : base(x => x.Id == id)
         {
             AddInclude(x => x.ProductType);
             AddInclude(x => x.ProductBrand);
