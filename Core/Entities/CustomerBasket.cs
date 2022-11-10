d42d361e6d9ef6def80cb47b8cb9881daa65e435
A-rielox <arielox.ag@gmail.com>
2022-11-10 12:04:08 -0600

    lista seccion 13

---
+++ Core/Entities/CustomerBasket.cs
@@ -0,0 +1,16 @@
+﻿namespace Core.Entities
+{
+    public class CustomerBasket
+    {
+        public CustomerBasket()
+        {// este lo necesita Redis
+        }
+        public CustomerBasket(string id)
+        {
+            Id = id;
+        }
+
+        public string Id { get; set; }
+        public List<BasketItem> Items { get; set; } = new List<BasketItem>();
+    }
+}
